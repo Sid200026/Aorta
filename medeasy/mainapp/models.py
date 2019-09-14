2427088391f78098150658d442cde0cb9087c235
dxmxnlord <rishikesh.sriram06@gmail.com>
2019-09-14 12:13:09 +0530

    creating user database

--- medeasy/mainapp/models.py
+++ medeasy/mainapp/models.py
@@ -1,3 +1,34 @@
 from django.db import models
+from users.models import CustomUser
+import datetime
 
-# Create your models here.
+class Patient(models.Model):
+	user=models.OneToOneField(CustomUser,on_delete=models.CASCADE)
+	firstname=models.CharField(max_length=50,blank=False)
+	lastname=models.CharField(max_length=50,blank=False)
+	dateofbirth=models.CharField(max_length=10,blank=False)
+	address=models.CharField(max_length=100,blank=False)
+	phonenumber=models.CharField(max_length=10)
+	profilepicture=models.ImageField(upload_to='profileimages/')
+	age=models.IntegerField()
+	weight=models.IntegerField()
+	height=models.IntegerField()
+	sex=models.CharField(max_length=6,choices=[('Male','Male'),('Female','Female'),('Other','Other')],blank=False)
+
+	def __str__(self):
+		return user.username
+
+class Doctor(models.Model):
+
+	user=models.OneToOneField(CustomUser,on_delete=models.CASCADE)
+	firstname=models.CharField(max_length=50,blank=False)
+	lastname=models.CharField(max_length=50,blank=False)
+	profilepicture=models.ImageField(upload_to='profileimages/')
+	phonenumber=models.CharField(max_length=10,blank=False)
+	degrees=models.CharField(max_length=50,blank=False)
+	iscertified=models.BooleanField(default=False)
+	registrationno=models.CharField(max_length=30)
+	bio=models.CharField(max_length=100)
+
+	def __str__(self):
+		return user.username
